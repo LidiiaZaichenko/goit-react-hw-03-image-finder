72b81fc31b65d6a9849510e2fd8580504765a54f
LidiiaZaichenko <119893988+LidiiaZaichenko@users.noreply.github.com>
2023-10-17 22:30:23 +0300

    dlt

--- src/api.js
+++ src/api.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 axios.defaults.baseURL = 'https://pixabay.com/api/';
 
 export const getGallery = async () => {
-  const API_KEY = '40095874-b5c4c3ccb4b604f2bda494f4f';
   const response = await axios.get(
     '?q=cat&page=1&key=40095874-b5c4c3ccb4b604f2bda494f4f&image_type=photo&orientation=horizontal&per_page=12'
   );
