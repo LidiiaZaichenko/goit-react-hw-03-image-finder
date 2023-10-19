68ec48a7ef0a8e7e4f3564d0b614cfa3169156a8
LidiiaZaichenko <119893988+LidiiaZaichenko@users.noreply.github.com>
2023-10-19 20:35:19 +0300

    api

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,11 +1,11 @@
 export const ImageGallery = ({ listImages }) => {
   return (
     <ul>
-  {listImages.hits.map(listImage => (
+  {/* {listImages.hits.map(listImage => (
   <li >
     <img src={listImage.pageURL} />
   </li>
-))}
+))} */}
     </ul>
   );
 };
