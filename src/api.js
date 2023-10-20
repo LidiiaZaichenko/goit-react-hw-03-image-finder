44b358d52ec2cd990a19fb4cb8e3f8bbbebe5eb2
LidiiaZaichenko <119893988+LidiiaZaichenko@users.noreply.github.com>
2023-10-20 16:50:26 +0300

    q

--- src/api.js
+++ src/api.js
@@ -4,7 +4,7 @@ const API_KEY = '40095874-b5c4c3ccb4b604f2bda494f4f';
 const BASE_URL = 'https://pixabay.com/api/?';
 const params = new URLSearchParams({
   key: API_KEY,
-  q:'cat',
+  q: 'cat',
   page: 1,
   image_type: 'photo',
   orientation: 'horizontal',
--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,10 +1,10 @@
 export const Searchbar = ({ onSubmit }) => {
   return (
     <div>
-      <header >
-        <form >
-          <button type="submit" >
-            <span >Search</span>
+      <header>
+        <form>
+          <button type="submit">
+            <span>Search</span>
           </button>
           <input
             type="text"
